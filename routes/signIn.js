df106ef6a6d4f5a02673990cce9e09c35335ca25
Sean Kelly <spkellydev@gmail.com>
2017-11-06 18:00:34 -0500

    auth dependencies

--- routes/signIn.js
+++ routes/signIn.js
@@ -1,4 +1,14 @@
-//pull in express
 const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const mongoose = require('mongoose')
+
+//Connect to mongoDB
+mongoose.connect('mongodb://testy:testy@ds239965.mlab.com:39965/companies');
+const db = mongoose.connection;
